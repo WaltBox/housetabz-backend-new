c776267ebb27d567118145684ceaf780745047c4
WaltBox <waltboxwell@gmail.com>
2024-10-13 16:11:30 -0500

    removed duplicate files

--- src/models/serviceRequestBundle.js
+++ src/models/serviceRequestBundle.js
@@ -1,25 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
-    const ServiceRequestBundle = sequelize.define('ServiceRequestBundle', {
-      status: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      houseId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      userId: {  // The roommate who submitted the request
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-    });
-  
-    ServiceRequestBundle.associate = (models) => {
-      ServiceRequestBundle.belongsTo(models.House, { foreignKey: 'houseId' });
-      ServiceRequestBundle.belongsTo(models.User, { foreignKey: 'userId', as: 'submitter' });
-      ServiceRequestBundle.hasMany(models.Task, { foreignKey: 'serviceRequestBundleId', as: 'tasks' });
-    };
-  
-    return ServiceRequestBundle;
+  const ServiceRequestBundle = sequelize.define('ServiceRequestBundle', {
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    houseId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    userId: { // The roommate who submitted the request
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    roommate_accepted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false, // Default to not accepted
+    },
+  });
+
+  ServiceRequestBundle.associate = (models) => {
+    ServiceRequestBundle.belongsTo(models.House, { foreignKey: 'houseId' });
+    ServiceRequestBundle.belongsTo(models.User, { foreignKey: 'userId', as: 'submitter' });
+    ServiceRequestBundle.hasMany(models.ServiceRequest, { foreignKey: 'serviceRequestBundleId' });
   };
-  
+
+  return ServiceRequestBundle;
+};
