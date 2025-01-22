bba561bb939f2d9d6fc91e9ae85dfd1bfcc54029
WaltBox <waltboxwell@gmail.com>
2025-01-21 20:52:18 -0600

    5 reqs fuck the bots

--- src/middleware/requestLimiter.js
+++ src/middleware/requestLimiter.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 // Rate limiter configuration
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per window
+  max: 5, // Limit each IP to 100 requests per window
   message: { message: 'Too many requests, please try again later.' },
 });
 
